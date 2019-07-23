e8b15760e3c83b1c029d7a954c6c4b95a571df62
LiFeng <459559238@qq.com>
2019-07-24 07:42:32 +0800

    提交MyBatis项目的初步修改版本

---
+++ MyBatisTestProject/src/main/java/dao/model/Student.java
@@ -0,0 +1,18 @@
+package dao.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private long id;
+
+    private String name;
+
+    private int grade;
+
+    private long classId;
+
+    private String extendParam;
+
+}
